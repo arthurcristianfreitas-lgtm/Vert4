756a7136a6cd01dd407edebd927e46bf2632bcc5
arthurcristianfreitas-lgtm <arthurcristianfreitas@gmail.com>
2026-05-26 16:38:41 -0300

    Remove server and build configurations from Vite

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
- server: {
-    port: 5173,
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      }
-    }
-  },
-  build: {
-    outDir: 'static',
-    emptyOutDir: true,
-    manifest: true,
-    rollupOptions: {
-      input: 'src/main.jsx',
-    }
-  }
 })
